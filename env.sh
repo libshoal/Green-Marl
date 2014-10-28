0aad3dc45db7e3f9ede923f5a150d661d612b247
Stefan Kaestle <stefan.kaestle@inf.ethz.ch>
2014-10-28 11:45:30 +0100

    Fixed path to libraries for new version of libshoal

--- env.sh
+++ env.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 PREFIX=$HOME/projects/gm
-export LD_LIBRARY_PATH=$PREFIX/shoal:$PREFIX/contrib/numactl-2.0.9:$PREFIX/contrib/papi-5.3.0/src:$PREFIX/contrib/papi-5.3.0/src/libpfm4/lib
+SHLPREFIX=$PREFIX/shoal/
+export LD_LIBRARY_PATH=$SHLPREFIX/shoal:$SHLPREFIX/contrib/numactl-2.0.9:$SHLPREFIX/contrib/papi-5.3.0/src:$SHLPREFIX/contrib/papi-5.3.0/src/libpfm4/lib
