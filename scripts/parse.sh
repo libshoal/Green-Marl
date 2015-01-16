3b8f8a2431b888abf7bea1ec7976cdd582c4c247
Stefan Kaestle <stefan.kaestle@inf.ethz.ch>
2015-01-16 10:51:49 +0100

    parse.sh: added check santify check for correctness of benchmarks
    Conflicts:
    	shoal

--- scripts/parse.sh
+++ scripts/parse.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+txtred='\e[0;31m' # Red
+txtgrn='\e[0;32m' # Green
+txtylw='\e[0;33m' # Yello
+txtrst='\e[0m'    # Text Reset
+
 function error() {
 	echo $1
 	exit 1
@@ -24,7 +29,7 @@ OVERVIEW=$(find $TMP -name 'run_all-overview-*')
 # CONFIGURATION
 # --------------------
 WORKLOAD="determinedAutomatically"
-MACHINE="determinedAutomatically"
+MACHINE="sgs-r815-03"
 # --------------------------------------------------
 
 function read_entry() {
@@ -46,6 +51,87 @@ function check_file() {
     echo "$1"
 }
 
+# --------------------------------------------------
+# Calculate
+#
+# wrapper for bc, using 8 digits after comma and passing everything
+# given as arguments to the function as arguments to bc
+#
+# --------------------------------------------------
+function calc() {
+    bc <<EOF
+scale=8
+$@
+quit
+EOF
+}
+
+# --------------------------------------------------
+# Make sure measurement was executing properly
+#
+# - output is correct
+# - time measurements sum up correctly
+#
+# arguments:
+# 1: file to check
+# --------------------------------------------------
+function check_measurement() {
+
+    CHECK_FILE=$1
+
+    NUM_RUNS=$(cat $CHECK_FILE | grep 'SHOAL.*initialization' | wc -l)
+    echo -n " -> Detected $NUM_RUNS runs -- "
+
+    NUM_SUCC=$(cat $CHECK_FILE | grep "result.*correct.*CRC.*correct" | wc -l)
+    echo -n " succ: $NUM_SUCC"
+
+    echo ""
+
+    SUM=0
+
+    # We currently use only "copy" and "comp" in the results.
+    # We need to make sure that we understand for all the additional costs how to account them for.
+
+    for title in "copy" "comp" "crc"; do
+
+	# extract measurements ..
+	CHECK_NUMS=$(cat $CHECK_FILE | \
+	    gawk -v TITLE="$title" '{ if ($0 ~ "^"TITLE ":") print $ 2}')
+
+	# run statistics
+	CHECK_STAT=$(echo "$CHECK_NUMS" | skstat.py)
+
+	# convert to array
+	CHECK_STAT_ARR=($CHECK_STAT)
+
+	# need measurement and stderr
+	RES=${CHECK_STAT_ARR[0]}
+	STDERR=${CHECK_STAT_ARR[1]}
+
+	# Make sure that stderr is less than 10% of measurement
+	if [[ $(calc "$STDERR/$RES<0.01") -eq 0 ]]; then echo -e $txtred "standard error too high" $txtrst; fi
+
+	# Sum up measurements
+	SUM=$(calc "$SUM+$RES")
+    done
+
+    # Make sure the Green Marl calculated total is similar to the sum of our break-down
+    GMTOTAL=$(cat $CHECK_FILE | gawk '/^gmtotal:/ { print $2 }' | skstat.py | cut -f2 -d' ')
+    echo -n " sum(copy+comp+total+crc) is $SUM, GMTOTAL is $GMTOTAL"
+
+    RATIO=$(calc "$GMTOTAL/$SUM")
+
+    # Check if the "unknown" time is less than 1%
+    RATIO_GOOD=1
+    if [[ $(calc "$RATIO>1.01") -eq 1 ]]; then RATIO_GOOD=0; fi
+    if [[ $(calc "$RATIO<0.99") -eq 1 ]]; then RATIO_GOOD=0; fi
+
+    echo -n " ratio is: "
+    if [[ $RATIO_GOOD -ne 1 ]]; then echo -n -e $txtred; fi
+
+    echo -e "$RATIO" $txtrst
+}
+
 function read_basic_info() {
 
     FILE=$1
@@ -56,8 +142,8 @@ function read_basic_info() {
     export WORKLOAD
 
     # Hostname: <name>
-    MACHINE=$(cat $FILE | awk '/^Hostname/ { print $2 }')
-    export MACHINE
+    # MACHINE=$(cat $FILE | awk '/^Hostname/ { print $2 }')
+    # export MACHINE
 }
 
 echo "Overview file is $OVERVIEW"
@@ -65,8 +151,12 @@ echo "Overview file is $OVERVIEW"
 basicInfo=0
 
 # Clear existing measurements
+# --------------------------------------------------
 while read line
 do
+    # Ignore lines starting with #
+    if (echo "$line" | grep '^#' ); then echo "Ignoring $line"; continue; fi
+
     FILE=$(read_entry "$line" 1)
     APP=$(read_entry "$line" 2)
     CONF=$(read_entry "$line" 3 | python -c "import re; import fileinput; print ''.join([ x for x in fileinput.input()[0] if re.match('\w', x)])")
@@ -98,8 +188,12 @@ do
 done < $OVERVIEW
 
 # Find measurements
+# --------------------------------------------------
 while read line
 do
+    # Ignore lines starting with #
+    if (echo "$line" | grep '^#' ); then echo "Ignoring $line"; continue; fi
+
     FILE=$(read_entry "$line" 1)
     APP=$(read_entry "$line" 2)
     CONF=$(read_entry "$line" 3 | python -c "import re; import fileinput; print ''.join([ x for x in fileinput.input()[0] if re.match('\w', x)])")
@@ -117,6 +211,8 @@ do
 
     check_file $F_COMP
     check_file $F_INIT
+
+    check_measurement $TMP/$FILE
 
     T_COMP=$(cat $TMP/$FILE | awk '/^comp/ { print $2 }' | skstat.py)
     T_INIT=$(cat $TMP/$FILE | awk '/^copy/ { print $2 }' | skstat.py)
