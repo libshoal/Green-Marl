a91096602354dbd22c2e7be307702898e73cf95a
Stefan Kaestle <stefan.kaestle@inf.ethz.ch>
2016-06-07 10:24:08 +0200

    Updated pygments lexer

--- etc/pygments/gm.py
+++ etc/pygments/gm.py
@@ -15,21 +15,27 @@ class GMLexer(RegexLexer):
     aliases = ['gm']
     filenames = ['*.gm']
 
-    keywords = ['While', 'Foreach', 'Sum', 'Do ',
-                'Procedure', '\.Nodes', '\.InNbrs', 'Node_Prop']
+    keywords = ['While', 'Foreach', 'Sum', 'Do ', 'Exist', 'Return',
+                'Procedure', '\.Nodes', '\.InNbrs', 'Node_Prop', '\.Nbrs' ]
     operators = ['\&\&', '<', '>', '\+=', '/', '@', '\+\+', '\+',
-                 '-', '\*', ',', '\.', '\|' ]
-    types = ['Double', 'Int', 'Graph']
+                 '-', '\*', ',', '\.', '\|', '!', '\?' ]
+    types = ['Double', 'Int', 'Graph', 'Bool', 'Long']
     punctuation = [ '{', '}', '\[', '\]', '\(', '\)', ';', ':', '=',  ]
-    buildin = ['OutDegree', 'NumNodes']
+    buildin = ['OutDegree', 'NumNodes', 'HasEdgeTo' ]
+    constants = [ '\+INF', 'True', 'False' ]
     
     tokens = {
-        'root': [
+        'root': \
+        [ ('//.*', Comment) ] +
+        [ ('%s' % s, Number.Constant ) for s in constants ] +
+        [
             (r'\s+', Text),
             (r'[0-9]+.[0-9]+', Number.Float),
             (r'[0-9]+', Number.Integer),
+            (r'\+INF', Number.Integer),
         ] +
         [ ('%s\(\)' % s, Name.Function) for s in buildin ] +
+        [ ('%s\(' % s, Name.Function) for s in buildin ] +
         [ ('%s' % s, Keyword) for s in keywords ] +
         [ ('%s' % s, Keyword.Type) for s in types ] +
         [ ('%s' % s, Operator) for s in operators ] +
